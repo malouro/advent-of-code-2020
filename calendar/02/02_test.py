74291482394da424965f7be918fc8860c17f86a3
Michael Louro <Michael.Louro@icims.com>
2020-12-14 12:00:04 -0500

    [Day 2] Part two solution & tests

--- calendar/02/02_test.py
+++ calendar/02/02_test.py
@@ -1,12 +1,39 @@
-from password_philosophy import solution as run
+from password_philosophy import solve as run_against_list
+from password_philosophy import check_password
+from password_philosophy import parse
 
-def test_password_philosophy():
-	example_data = [
-		'1-3 a: abcde',
-		'1-3 b: cdefg',
-		'2-9 c: ccccccccc'
-	]
+class TestPasswordPolicyPartOne:
+	policy = 1
 
-	result = run(example_data)
-	print(result)
-	assert result == 2
+	def test_with_list_data(self):
+		example_data = (
+			'1-3 a: abcde',
+			'1-3 b: cdefg',
+			'2-9 c: ccccccccc'
+		)
+
+		assert run_against_list(example_data, self.policy) == 2
+
+	def test_unit(self):
+		assert check_password(parse('1-3 a: abcde'), self.policy) == True
+		assert check_password(parse('1-3 b: cdefg'), self.policy) == False
+		assert check_password(parse('2-9 c: ccccccccc'), self.policy) == True
+
+
+class TestPasswordPolicyPartTwo:
+	policy = 2
+
+	def test_with_list_data(self):
+		example_data = (
+			'1-3 a: abcde',
+			'1-3 b: cdefg',
+			'2-9 c: ccccccccc'
+		)
+
+		assert run_against_list(example_data, self.policy) == 1
+
+	def test_unit(self):
+		assert check_password(parse('1-3 a: abcde'), self.policy) == True
+		assert check_password(parse('1-3 b: cdefg'), self.policy) == False
+		assert check_password(parse('2-9 c: ccccccccc'), self.policy) == False
+
--- calendar/02/password_philosophy.py
+++ calendar/02/password_philosophy.py
@@ -1,43 +1,108 @@
 from os import path
 from sys import argv
 
-def solution(data):
-	# Number of valid passwords
-	valid_passes = 0
+"""Enum of valid password policy values"""
+valid_password_policies = (1, 2)
 
-	for item in data:
-		item = item.split(' ')
+def parse(str):
+	"""
+	Parse info from given password data.
+	[ requirement, letter_to_check_for, password]
+
+	```py
+	# This:
+	parse("1-3 a: abcde")
+
+	# Returns:
+	{ 'item': ['1-3', 'a:', 'abcde'],
+	'req': [1, 3],
+	'letter': 'a',
+	'password': 'abcde' }
+	```
+	"""
+	item = str.split(' ')
+
+	return {
+		'item': item,
+		'req': list(map(int, item[0].split('-'))),
+		'letter': item[1][0],
+		'password': item[2]
+	}
+
+def check_password(data, policy):
+	"""
+	Implement password policy against given password data.
+
+	Returns `True` for valid password, `False` for invalid
+	"""
+	item = data['item']
+	req = data['req']
+	letter = data['letter']
+	password = data['password']
+
+	if policy == valid_password_policies[0]:
+		"""
+		Policy 1:
+
+		`(X-Y)` ranged occurrences of a given letter must exist within the password.
+		(No more, no less)
+		"""
 		count = 0
-
-		# Parse info from data...
-		# [ requirement, letter to check for, password]
-		# ie:
-		# [ '1-3', 'a:', 'abcde' ] => passes, valid_passes++
-		req = list(map(int, item[0].split('-')))
-		letter = item[1][0]
-		password = item[2]
-
-		# Check the password!
 		for char in password:
 			if char == letter:
 				if count == req[1]:
 					count = 0
 					break
 				count += 1
-
 		if count >= req[0]:
-			valid_passes += 1
 			print(f'Valid pass found! {item}')
+			return True
+		return False
+
+	elif policy == valid_password_policies[1]:
+		"""
+		Policy 2:
+
+		Letter must exist at one, and only one, of the positions given with (non-zero indexed) `req`
+
+		Eg:
+		```
+		1-3 a: abcdef => **Passes** because 'a' exists at (1) and NOT (3)
+		1-5 c: cccccc => **Fails** because 'c' exists at BOTH (1) and (3)
+		1-3 f: eeeeee => **Fails** because 'f' does NOT exist at (1) OR (3)
+		```
+		"""
+		if (password[req[0] - 1] == letter) != (password[req[1] - 1] == letter):
+			print(f'Valid pass found! {item}')
+			return True
+		return False
+
+	# policy number not supported
+	else:
+		return Exception(f'Invalid password policy {policy} given. Valid options include: {valid_password_policies}')
+
+def solve(password_data, policy):
+	valid_passes = 0
+
+	for item in password_data:
+		parsed_data = parse(item)
+		if check_password(parsed_data, policy) == True:
+			valid_passes += 1
 
 	return valid_passes
 
 
 if __name__ == "__main__":
 	del argv[0]
+	policy = 1
 
 	if path.isfile(argv[0]):
 		data = list(map(str, open(argv[0]).read().split('\n')))
+		if argv.__len__() > 1:
+			policy = int(argv[1])
+			print(f'Using given policy number of {policy}.')
 	else:
 		raise Exception('Bad data, check your input file.')
 
-	print(f'Valid password total: {solution(data)}')
+	results = solve(data, policy)
+	print(f'✅ Valid password total: {results}')
