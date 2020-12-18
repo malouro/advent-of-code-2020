22358973baa060023d442643c7981d9ae433b797
Mike A. Louro <mike.a.louro@gmail.com>
2020-12-18 17:13:08 -0500

    [Day 4] Part two solution & test
    - Still need to work out why the solution wasn't correct
    - [ ] Check that all test cases were covered?

--- calendar/04/process_passport.py
+++ calendar/04/process_passport.py
@@ -12,19 +12,90 @@ REQUIRED_FIELDS = [
 	'pid'
 ]
 
+MIN_BIRTH_YEAR = 1920
+MAX_BIRTH_YEAR = 2002
+
+MIN_ISSUE_YEAR = 2010
+MAX_ISSUE_YEAR = 2020
+
+MIN_EXP_YEAR = 2020
+MAX_EXP_YEAR = 2030
+
+MIN_HEIGHT_CM = 150
+MAX_HEIGHT_CM = 193
+MIN_HEIGHT_IN = 59
+MAX_HEIGHT_IN = 76
+
+POSSIBLE_EYE_COLORS = 'amb|blu|brn|gry|grn|hzl|oth'
+
+
+def parse_field(field):
+	"""
+	Parses a field from the passport.
+	Returns `True` for a valid passport field entry.
+
+	> Note: A `field` must be a tuple/list of two elements: the field `type` and the field's `value`
+	"""
+	field_type = field[0]
+	field_value = field[1]
+
+	# Birth Year
+	if field_type == 'byr':
+		return MIN_BIRTH_YEAR <= int(field_value) <= MAX_BIRTH_YEAR
+	# Issue Year
+	if field_type == 'iyr':
+		return MIN_ISSUE_YEAR <= int(field_value) <= MAX_ISSUE_YEAR
+	# Expiration Year
+	if field_type == 'eyr':
+		return MIN_EXP_YEAR <= int(field_value) <= MAX_EXP_YEAR
+	# Height (No dwarves or basketball players allowed, apparently)
+	if field_type == 'hgt':
+		if 'cm' in field_value:
+			return MIN_HEIGHT_CM <= int(field_value[:-2]) <= MAX_HEIGHT_CM
+		if 'in' in field_value:
+			return MIN_HEIGHT_IN <= int(field_value[:-2]) <= MAX_HEIGHT_IN
+		return False
+	# Hair Color (no rgb or hsl hair, alright?)
+	if field_type == 'hcl':
+		return bool(re.fullmatch('#[0-9A-Fa-f]{6}', field_value))
+	# Eye color
+	if field_type == 'ecl':
+		return bool(re.fullmatch(POSSIBLE_EYE_COLORS, field_value))
+	# Passport ID
+	if field_type == 'pid':
+		return bool(re.fullmatch('[0-9]{9}', field_value))
+	return False
+
 def parse_passport(passport):
 	"""
 	Parses a single passport for all necessary info.
 	Returns `True` for a valid passport.
 	"""
-	data_points = re.split(r'\n|\s+', passport)
-	fields = sorted(list(map(lambda x : x[:3], data_points)))
+	data_points = re.split(r'[\n\s]+', passport)
+	fields = list(map(lambda x: ( x[:3], x[4:] ), data_points))
 
-	if 'cid' in fields:
-		fields.remove('cid')
-	if fields == REQUIRED_FIELDS:
-		return True
-	return False
+	i = 0
+	for field in fields:
+		# Ignore field if type is "cid" by removing from list
+		if field[0] == 'cid':
+			del fields[i]
+			continue
+		# Parse field and its value
+		if not parse_field(field):
+			print(f'Field failed: {field} ❌')
+			return False
+		i += 0
+
+	# Now check that the fields supplied match requirements
+	fields_given = sorted(list(map(lambda x: x[0], fields)))
+
+	valid_passport = fields_given == REQUIRED_FIELDS
+
+	if valid_passport:
+		print(f'Passport valid: {passport} ✅')
+	else:
+		print(f'Passport invalid: {fields_given} ❌')
+	return valid_passport
 
 def validate_passports(passport_data):
 	"""
@@ -32,6 +103,7 @@ def validate_passports(passport_data):
 	of passport data.
 	"""
 	passports = list(passport_data.split('\n\n'))
+	print(f'Total passports to parse: {passports.__len__()}')
 	valid_count = 0
 
 	for passport in passports:
@@ -47,4 +119,4 @@ if __name__ == "__main__":
 	else:
 		raise Exception('Bad input data, or file doesn\'t exist.')
 
-	print(validate_passports(input_data))
+	print(f'Valid passports: {validate_passports(input_data)}')
--- calendar/04/test_04.py
+++ calendar/04/test_04.py
@@ -1,11 +1,80 @@
-from process_passport import parse_passport, validate_passports, REQUIRED_FIELDS
+from process_passport import parse_field, parse_passport, validate_passports, REQUIRED_FIELDS
 
 class TestProcessPassports:
-	good_passports = ()
-	bad_passports = (
-		f'''{REQUIRED_FIELDS[0]}:1492'''
-	)
+	good_passports = [
+		f'''cid:US
+pid:123456789
+eyr:2025
+iyr:2015
+byr:1969
+hcl:#ffee00
+ecl:hzl
+hgt:170cm'''
+	]
+	bad_passports = [
+		# Bad birth year; everything else OK
+		f'''cid:US
+		pid:123456789
+		eyr:2025
+		iyr:2015
+		hcl:#ffee00
+		ecl:hzl
+		hgt:170cm
+		{REQUIRED_FIELDS[0]}:1492''',
+		'', # Empty passport
+	]
 
-	def test_parse_passports(self):
+	good_eye_colors = ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth')
+	bad_eye_colors = ('prp', 'ylw', 'sharingan')
+
+	def test__parse_passports(self):
+		for good_passport in self.good_passports:
+			assert parse_passport(good_passport)
 		for bad_passport in self.bad_passports:
-			assert parse_passport(bad_passport) == False
+			assert not parse_passport(bad_passport)
+
+	def test__parse_field__birth_year(self):
+		assert parse_field(('byr', '2000'))
+		assert not parse_field(('byr', '1492'))
+
+	def test__parse_field__issue_year(self):
+		assert not parse_field(('iyr', 2000))
+		assert not parse_field(('iyr', 2021))
+		assert parse_field(('iyr', 2015))
+
+	def test__parse_field__issue_year(self):
+		assert not parse_field(('eyr', 2010))
+		assert not parse_field(('eyr', 2031))
+		assert parse_field(('eyr', 2025))
+
+	def test__parse_field__height(self):
+		# Centimeters
+		assert not parse_field(('hgt', '200cm'))
+		assert not parse_field(('hgt', '140cm'))
+		assert parse_field(('hgt', '170cm'))
+
+		# Inches
+		assert not parse_field(('hgt', '50in'))
+		assert not parse_field(('hgt', '100in'))
+		assert parse_field(('hgt', '69in'))
+
+		# Fail other units
+		assert not parse_field(('hgt', '0.8m'))
+
+	def test__parse_field__hair_color(self):
+		assert parse_field(('hcl', '#abcdef')) # Typical hex code -- passes
+		assert parse_field(('hcl', '#01cdEF')) # More wacky hex code (mixed letters, capitals, numbers) -- passes
+		assert not parse_field(('hcl', 'rgb(1,2,3)'))
+		assert not parse_field(('hcl', 'blonde'))
+		assert not parse_field(('hcl', 'abcdef')) # Hex code without '#' -- fails
+
+	def test__parse_field__eye_color(self):
+		for good_eye_color in self.good_eye_colors:
+			assert parse_field(('ecl', good_eye_color))
+		for bad_eye_color in self.bad_eye_colors:
+			assert not parse_field(('ecl', bad_eye_color))
+
+	def test__parse_field__passport_id(self):
+		assert parse_field(('pid', '000333999'))
+		assert not parse_field(('pid', '123'))
+		assert not parse_field(('pid', '000044448888'))
